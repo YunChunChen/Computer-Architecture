94a11f36b4f7c5b518efc2983702bf51df55545c
chenyunjun <chenyunjun@chenyunjun.local>
2017-03-17 20:41:20 +0800

    modify indentation

--- hw1/ALU/Behavior/alu.v
+++ hw1/ALU/Behavior/alu.v
@@ -23,21 +23,21 @@ module alu( ctrl, x, y, carry, out);
 
     // x and y should lie within 127 and -128 
     always@(x or y or ctrl) begin
-	         if (ctrl == 4'b0000) {carry, out} = {x[7], x} + {y[7], y};
+             if (ctrl == 4'b0000) {carry, out} = {x[7], x} + {y[7], y};
         else if (ctrl == 4'b0001) {carry, out} = {x[7], x} - {y[7], y};
- 	    else if (ctrl == 4'b0010) out = x & y;
+        else if (ctrl == 4'b0010) out = x & y;
         else if (ctrl == 4'b0011) out = x | y;
-	    else if (ctrl == 4'b0100) out = ~x;
+        else if (ctrl == 4'b0100) out = ~x;
         else if (ctrl == 4'b0101) out = x ^ y;
-	    else if (ctrl == 4'b0110) out = ~(x | y);
+        else if (ctrl == 4'b0110) out = ~(x | y);
         else if (ctrl == 4'b0111) out = y << x[2:0];
-	    else if (ctrl == 4'b1000) out = y >> x[2:0];    
+        else if (ctrl == 4'b1000) out = y >> x[2:0];    
         else if (ctrl == 4'b1001) out = {x[7], x[7:1]};  
-	    else if (ctrl == 4'b1010) out = {x[6:0], x[7]};
+        else if (ctrl == 4'b1010) out = {x[6:0], x[7]};
         else if (ctrl == 4'b1011) out = {x[0], x[7:1]};
-	    else if (ctrl == 4'b1100) out = ( x == y ) ? 1 : 0;
+        else if (ctrl == 4'b1100) out = ( x == y ) ? 1 : 0;
         else if (ctrl == 4'b1101) out = 0;
-	    else if (ctrl == 4'b1110) out = 0;
+        else if (ctrl == 4'b1110) out = 0;
         else if (ctrl == 4'b1111) out = 0;
     end 	
 
