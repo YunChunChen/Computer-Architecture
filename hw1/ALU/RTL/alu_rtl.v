bbf9347682fb10d6c0eb4146ba884ae0ffb77de1
chenyunjun <chenyunjun@chenyunjun.local>
2017-03-17 20:37:06 +0800

    modify indentation

--- hw1/ALU/RTL/alu_rtl.v
+++ hw1/ALU/RTL/alu_rtl.v
@@ -23,19 +23,19 @@ module alu_rtl( ctrl, x, y, carry, out);
  
     // x and y should lie with 127 to -128	
     assign {carry, out} = (ctrl == 4'b0000) ? {x[7], x} + {y[7], y}:	
-        	              (ctrl == 4'b0001) ? {x[7], x} - {y[7], y}:	
-		                  (ctrl == 4'b0010) ? x & y:
-		                  (ctrl == 4'b0011) ? x | y:
-          		          (ctrl == 4'b0100) ? ~x:
-	   	                  (ctrl == 4'b0101) ? x ^ y:
-		                  (ctrl == 4'b0110) ? ~(x|y):		
-	   	                  (ctrl == 4'b0111) ? y << x[2:0]:		
-	   	                  (ctrl == 4'b1000) ? y >> x[2:0]:		
-	   	                  (ctrl == 4'b1001) ? {x[7], x[7:1]}:		
+                          (ctrl == 4'b0001) ? {x[7], x} - {y[7], y}:
+                          (ctrl == 4'b0010) ? x & y:
+                          (ctrl == 4'b0011) ? x | y:
+                          (ctrl == 4'b0100) ? ~x:
+                          (ctrl == 4'b0101) ? x ^ y:
+                          (ctrl == 4'b0110) ? ~(x|y):		
+                          (ctrl == 4'b0111) ? y << x[2:0]:		
+                          (ctrl == 4'b1000) ? y >> x[2:0]:		
+                          (ctrl == 4'b1001) ? {x[7], x[7:1]}:		
                           (ctrl == 4'b1010) ? {x[6:0], x[7]}:
-		                  (ctrl == 4'b1011) ? {x[0], x[7:1]}:
-		                  (ctrl == 4'b1100) ? ( ( x == y ) ? 1 : 0 ):
-		                  (ctrl == 4'b1101) ? 0 : 
+                          (ctrl == 4'b1011) ? {x[0], x[7:1]}:
+                          (ctrl == 4'b1100) ? ( ( x == y ) ? 1 : 0 ):
+                          (ctrl == 4'b1101) ? 0 : 
                           (ctrl == 4'b1110) ? 0 :  
-			         	                      0 ;
+                                              0 ;
 endmodule
