bad5b468c100f39eea43bf5e78edaa7470615df1
YunChunChen <b03901148@ntu.edu.tw>
2017-04-20 23:16:16 +0800

    small revision

--- hw1/ALU/RTL/alu_rtl.v
+++ hw1/ALU/RTL/alu_rtl.v
@@ -20,8 +20,8 @@ module alu_rtl( ctrl, x, y, carry, out);
     input  [3:0] ctrl;
     input  [7:0] x;
     input  [7:0] y;
-    output       carry;
     output [7:0] out;
+    output       carry;
  
     // x and y should lie with 127 to -128	
     assign {carry, out} = (ctrl == 4'b0000) ? {x[7], x} + {y[7], y}:	
@@ -38,6 +38,5 @@ module alu_rtl( ctrl, x, y, carry, out);
                           (ctrl == 4'b1011) ? {x[0], x[7:1]}:
                           (ctrl == 4'b1100) ? ( ( x == y ) ? 1 : 0 ):
                           (ctrl == 4'b1101) ? 0 : 
-                          (ctrl == 4'b1110) ? 0 :  
-                                              0 ;
+                          (ctrl == 4'b1110) ? 0 ;
 endmodule
