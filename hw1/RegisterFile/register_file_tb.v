c29e13a75be9888494df08371fb4664437be07a3
YunChunChen <b03901148@ntu.edu.tw>
2017-04-20 23:06:13 +0800

    indentation

--- hw1/RegisterFile/register_file_tb.v
+++ hw1/RegisterFile/register_file_tb.v
@@ -23,7 +23,7 @@ module register_file_tb;
 	
     parameter  nPattern = 10;
 
-    reg		   rst_n;
+    reg        rst_n;
     reg	 [7:0] patterns[0:nPattern];
     
     integer    i, error_num, counter;   
@@ -37,76 +37,75 @@ module register_file_tb;
     end
    
     always begin
-	    #(`HCYCLE) Clk = ~Clk;	//generaing clk
+        #(`HCYCLE) Clk = ~Clk; //generaing clk
     end
 
-    initial begin 		// Stimulus patterns
+    initial begin // Stimulus patterns
         Clk = 1;
-	    rst_n = 1;
-	    error_num = 0;
+        rst_n = 1;
+        error_num = 0;
 
-	    for( i = 0; i < nPattern ; i = i + 1 ) begin
-		    patterns[i] = {$random}%256;
+        for( i = 0; i < nPattern ; i = i + 1 ) begin
+            patterns[i] = {$random}%256;
         end
 	
-	    #( `CYCLE * 0.5 ) rst_n = 0;
-	    #( `CYCLE ) rst_n = 1;
-	    #( 30 * `CYCLE ) 
+        #( `CYCLE * 0.5 ) rst_n = 0;
+        #( `CYCLE ) rst_n = 1;
+        #( 30 * `CYCLE ) 
 	
         if(!error_num)
-		    $display("\nCongratulations!! Your Verilog Code is correct!!\n");
-	    else
-		    $display("\nYour Verilog Code has %d errors. \n",error_num);
-	    $finish;
+            $display("\nCongratulations!! Your Verilog Code is correct!!\n");
+        else
+            $display("\nYour Verilog Code has %d errors. \n",error_num);
+        $finish;
     end
 
     always@(posedge Clk or negedge rst_n) begin
-	    if( !rst_n ) begin
-		    counter = 0;
-			error_num = 0;
-	    end
-	    else begin
-		    if (counter != 8 && counter != 16) begin
-			    if (counter<8) begin
-				    RX  <= 0;
-				    RY  <= 0;
-				    RW  <= counter[2:0];
-				    WEN <= 1;
-				    busW <= patterns[counter];
-			    end
-			    else if (counter<16) begin
-				    RX  <= counter[2:0];
-				    RY  <= 0;
-				    RW  <= counter[2:0];
-				    WEN <= 0;
-				    busW<= 0;
-				
-				if (counter==9 && busX!==8'd0) begin
-					error_num = error_num + 1;
-					$display("error1 QQ!!! %b , %d\n",busX,counter);
-				end	
-				else if (counter!=9 && busX!==patterns[counter-9]) begin
-					error_num = error_num + 1;
-					$display("error2 QQ!!! %b , %d\n",busX,counter);
-				end
-			end
+        if( !rst_n ) begin
+            counter = 0;
+            error_num = 0;
+        end
+        else begin
+            if (counter != 8 && counter != 16) begin
+                if (counter<8) begin
+                    RX  <= 0;
+                    RY  <= 0;
+                    RW  <= counter[2:0];
+                    WEN <= 1;
+                    busW <= patterns[counter];
+                end
+                else if (counter<16) begin
+                    RX  <= counter[2:0];
+                    RY  <= 0;
+                    RW  <= counter[2:0];
+                    WEN <= 0;
+                    busW<= 0;
+                end
+                if (counter==9 && busX!==8'd0) begin
+                    error_num = error_num + 1;
+                    $display("error1 QQ!!! %b , %d\n",busX,counter);
+                end	
+                else if (counter!=9 && busX!==patterns[counter-9]) begin
+                    error_num = error_num + 1;
+                    $display("error2 QQ!!! %b , %d\n",busX,counter);
+                end
+            end
 			else if (counter<24) begin
-				RX  <= 0;
-				RY  <= counter[2:0];
-				RW  <= counter[2:0];
-				WEN <= 0;
-				busW<= 0;
-				
+                RX  <= 0;
+                RY  <= counter[2:0];
+                RW  <= counter[2:0];
+                WEN <= 0;
+                busW<= 0;
 				if (counter==17 && busY!==8'd0) begin
-					error_num = error_num + 1;
-					$display("error3 QQ!!! %b , %d\n",busY,counter);
-				end
-				else if (counter!=17 && busY!==patterns[counter-17]) begin
-					error_num = error_num + 1;
-					$display("error4 QQ!!! %b , %d\n",busY,counter);
-				end
-			end		
-		end
-		counter = counter+1;
-	end
+                    error_num = error_num + 1;
+                    $display("error3 QQ!!! %b , %d\n",busY,counter);
+                end
+                else if (counter!=17 && busY!==patterns[counter-17]) begin
+                    error_num = error_num + 1;
+                    $display("error4 QQ!!! %b , %d\n",busY,counter);
+                end
+            end		
+        end
+        counter = counter+1;
+    end
 endmodule
